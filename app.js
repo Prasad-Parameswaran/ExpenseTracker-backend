61b8e58d707c0c56c7fed591b4f4774adf40fceb
Prasad P <prasadparameswaran1234@gmail.com>
2025-11-18 07:56:24 +0530

    feat: simplify CORS configuration in app.js

--- app.js
+++ app.js
@@ -11,11 +11,7 @@ const reportRoutes = require('./routes/reportRoutes');
 
 
 const app = express();
-app.use(
-    cors({
-        origin: process.env.CLIENT_ORIGIN
-    })
-);
+app.use(cors())
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
