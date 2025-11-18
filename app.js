b8eb588796c7e7d17fac4acb4040e537162e3a6d
Prasad P <prasadparameswaran1234@gmail.com>
2025-11-18 15:31:35 +0530

    refactor: streamline CORS middleware by consolidating header settings

--- app.js
+++ app.js
@@ -11,14 +11,12 @@ const reportRoutes = require('./routes/reportRoutes');
 
 
 const app = express();
+app.use(cors({ origin: "", methods: "", allowedHeaders: "*" }));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 app.use(express.json());
 
